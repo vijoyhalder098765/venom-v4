c6aa490e0533c185e96109bdd6b92d6bb79e603f
vijoyhalder098765 <haldervijoy594@gmail.com>
2024-09-01 02:28:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7478620706:AAH5eonr7oSA1QDTlwXOe6BdpP22t3dWqp8")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5715358718")
 
 
 
